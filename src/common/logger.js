680287fc5e39e73833b4896be70c4ac608e8e2e3
Devon <devonlee111@gmail.com>
2023-08-18 09:33:44 -0700

    Append to Message Log File

--- src/common/logger.js
+++ src/common/logger.js
@@ -10,17 +10,22 @@ module.exports = {
 		channel = message.channelId;
 		logMsg = `${message.createdAt.toString()}: ${message.author.username}: ${
 			message.content
-		}`;
+		}\n`;
 		logFileDir = `${MUSE_LOG_DIR}${DISCORD_MESSAGES_LOG_SUBDIR}${channel}`;
 		logFile = `${MUSE_LOG_DIR}${DISCORD_MESSAGES_LOG_SUBDIR}${channel}/${DISCORD_MESSAGES_LOG_FILE}`;
+
 		if (!fs.existsSync(logFileDir)) {
 			fs.mkdir(logFileDir, (err) => {
-				console.log("failed to create new directory for discord message logging: ", err);
+				console.log(
+					"failed to create new directory for discord message logging: ",
+					err
+				);
 				return;
-			})
+			});
 		}
+
 		try {
-			fs.writeFileSync(logFile, logMsg);
+			fs.appendFileSync(logFile, logMsg);
 		} catch (err) {
 			console.log(err);
 		}
